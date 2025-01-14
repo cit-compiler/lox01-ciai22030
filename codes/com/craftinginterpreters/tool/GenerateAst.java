21c55e63d882aea04ee11704f527353d0afc06fa
ciai22030 <ciai22030@g.nihon-u.ac.jp>
2025-01-14 12:20:11 +0900

    add loxfile

--- codes/com/craftinginterpreters/tool/GenerateAst.java
+++ codes/com/craftinginterpreters/tool/GenerateAst.java
@@ -15,15 +15,16 @@ public class GenerateAst {
         defineAst(outputDir, "Expr", Arrays.asList(
             "Assign   : Token name, Expr value",
                 "Binary   : Expr left, Token operator, Expr right",
-                 "Grouping : Expr expression",
-                 "Literal  : Object value",
-                 "Unary    : Token operator, Expr right",
-                 "Variable : Token name"
+                "Grouping : Expr expression",
+                "Literal  : Object value",
+                "Unary    : Token operator, Expr right",
+                "Variable : Token name"
         ));
         defineAst(outputDir, "Stmt", Arrays.asList(
-            "Expression : Expr expression",
-                 "Print      : Expr expression",
-                 "Var        : Token name, Expr initializer"
+            "Block      : List<Stmt> statements",
+                "Expression : Expr expression",
+                "Print      : Expr expression",
+                "Var        : Token name, Expr initializer"
         ));
 
     }
